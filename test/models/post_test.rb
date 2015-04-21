b6dd50626b629524537296d65bc123f7352c368b
Humberto Peña <humbertojpt@outlook.com>
2015-04-21 20:09:48 +0000

    funtional message with errors

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the a post requires content" do
+   		post = Post.new
+   		assert !post.save, "The post was save without content"
+   
+   		# puts "--Errors--"
+   		# post.errors.full_messages.each { |msg| puts msg}
+   end
+   test "that a post's content is at least 6 letters" do
+	   	post = Post.new
+	   	post.content = "short"
+
+	   	assert !post.save, "The post's content was saved without 6 letters"
+   end
+
+   test "that a post has a user id" do
+	   	post = Post.new
+	   	post.content = "Hello word"
+
+	   	assert !post.save, "The post's content was saved without user id"
+   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the a user requires email and password" do
+   		user = User.new
+   		assert !user.save, "The user was save without email and password"
+   
+   		#puts "--Errors--"
+   		#users.errors.full_messages.each { |msg| puts msg}
+   end
+
+   test "the a user have a unique email" do
+   		user = User.new
+   		user.email users(:one).email
+
+   		assert !user.save, "The user was whit a repeted email"
+   end
+
+   test "the a user have a name" do
+   		user = User.new
+   		user.email = "juan.perez@gmail.com"
+   		user.password = "12345678"
+
+   		assert !user.save, "The user was without a name"
+   
+   		# puts "--Errors--"
+   		# post.errors.full_messages.each { |msg| puts msg}
+   end
 end
